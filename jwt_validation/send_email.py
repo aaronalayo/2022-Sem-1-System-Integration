9070cc7fd75422cf40870ccc035367085c895a54
aaronalayo <aaron.aa@me.com>
2022-04-20 09:11:25 +0200

    Added send email

---
+++ jwt_validation/send_email.py
@@ -0,0 +1,52 @@
+#   https://realpython.com/python-send-email/
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import random
+
+sender_email = "editoraaron@gmail.com"
+receiver_email = "editoraaron@gmail.com"
+password = "Habana1959#"
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+random_auth_code = str(random.randint(100000, 999999))
+
+
+# Create the plain-text and HTML version of your message
+text = """\
+Hi,
+Thank you.
+"""
+
+html = f"""\
+<html>
+  <body>
+    <p>
+      Hi, your verification code is: {random_auth_code}
+    </p>
+  </body>
+</html>
+"""
+
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+message.attach(part2)
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    try:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    except Exception as ex:
+        print(ex)
